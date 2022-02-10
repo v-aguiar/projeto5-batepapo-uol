921cfcb472d0e6dc334ca649330de040edacb0dd
v-aguiar <v.aguiar.dev@gmail.com>
2022-02-10 15:28:18 -0300

    Allow messages to be sent to the API and shows them on screen. Also ensure private messages are only displayed for the sender and receiver

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
-﻿const enterRoom = {
-  username: '',
+﻿let username = '';
+
+const user = {
   stayLoggedInterval: 0,
 
   sendUserName() {
     const name = prompt("Qual o seu lindo nome?")
-    enterRoom.username = name
+    username = name
 
     const request = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", {name: name})
     request.then(this.keepUserLoggedIn)
@@ -18,30 +19,56 @@
   },
 
   keepUserLoggedIn(response) {
-
-    console.log(response)
-
     this.stayLoggedInterval = setInterval(() => {
-      const request = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {name: enterRoom.username})
-      request.then((response) => {
+      const request = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {name: username})
+      request.then(() => {
         console.log("still logged")
       })
     }, 4500)
+  },
+
+  getAllLoggedInUsers() {
+    const request = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants")
+    // request.then(user.isUserInRoom)
   }
 }
 
 const chat = {
   getMessages() {
     const request = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+
     request.then(this.populateChat)
   },
 
   populateChat(response) {
-    response.data.forEach((message) => {
-      const ifPrivateMessage = message.type === 'private_message' ? 'reservadamente' : ''
-      const ifPublicMessage = message.type === 'message' ? `${ifPrivateMessage} para <strong> ${message.to}:</strong>` : ''
+    const messagesContainer = document.querySelector(".messages")
+    messagesContainer.innerHTML = ''
 
-      const messageStructure = `
+    response.data.forEach((message) => {
+      const messageStructure = chat.buildMessageStructure(message)
+      const messageDiv = document.createElement('div')
+
+      if(message.type === 'private_message') {
+        if((username === message.from) || (username === message.to)) {
+          messageDiv.classList.add("message", `--${message.type}`)
+          messageDiv.innerHTML = messageStructure
+          messagesContainer.appendChild(messageDiv)
+          messageDiv.scrollIntoView()
+        }
+      } else {
+        messageDiv.classList.add("message", `--${message.type}`)
+        messageDiv.innerHTML = messageStructure
+        messagesContainer.appendChild(messageDiv)
+        messageDiv.scrollIntoView()
+      }
+    })
+  },
+
+  buildMessageStructure(message) {
+    const ifPrivateMessage = message.type === 'private_message' ? 'reservadamente' : ''
+    const ifPublicMessage = message.type === 'message' ? `${ifPrivateMessage} para <strong> ${message.to}:</strong>` : ''
+
+    const structure = `
         <p>
           <time class="time">${message.time}</time>
           <span class="who"><strong>${message.from}</strong> ${ifPublicMessage}</span>
@@ -49,23 +76,28 @@ const chat = {
         </p>
       `
 
-      const messagesContainer = document.querySelector(".messages")
-      const messageDiv = document.createElement('div')
-
-      messageDiv.classList.add("message", `--${message.type}`)
-      messageDiv.innerHTML = messageStructure
-      messagesContainer.appendChild(messageDiv)
-      messageDiv.scrollIntoView()
-    })
-
+    return structure
   },
 
   sendMessage() {
+    const text = document.querySelector("footer textarea")
 
+    const messageData = {
+      from: username,
+      to: "Todos",
+      text: `${text.value}`,
+      type: "message"
+    }
+    const validateSentMessage = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", messageData)
+
+    text.value = ''
+
+    validateSentMessage.then(chat.getMessages)
+    validateSentMessage.catch(() => {window.location.reload()})
   }
 }
 
 // Declarations
 
 setInterval(() => {chat.getMessages()}, 3000)
-enterRoom.sendUserName()
+user.sendUserName()
